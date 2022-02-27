9bb32e711a40feae2d1c4ff421442b13a0290da9
Vinare <scrat-me@mail.ru>
2022-02-27 17:17:02 +0300

    Done

--- Classes.js
+++ Classes.js
@@ -1,19 +1,37 @@
 "use strict";
 
 var name_id = document.getElementById("name_id");
-  var surname_id = document.getElementById("surname_id");
-  var birth_date_id = document.getElementById("birth_date_id");
-  var phone_id = document.getElementById("phone_id");
-  var region_id = document.getElementById("region_id");
-  var selectedTown = region_id.options[region_id.selectedIndex].textContent;
-  var cathedra_id = document.getElementById("cathedra_id");
-  var cathedra_idValue = cathedra_id.value;
-  var position_id = document.getElementById("position_id");
-  var faculty_id = document.getElementById("faculty_id");
-  var course_id = document.getElementById("course_id");
-//   var id = getParam("id");
+var surname_id = document.getElementById("surname_id");
+var birth_date_id = document.getElementById("birth_date_id");
+var phone_id = document.getElementById("phone_id");
+var region_id = document.getElementById("region_id");
+var selectedTown = region_id.options[region_id.selectedIndex].textContent;
+var cathedra_id = document.getElementById("cathedra_id");
+var cathedra_idValue = cathedra_id.value;
+var position_id = document.getElementById("position_id");
+var faculty_id = document.getElementById("faculty_id");
+var course_id = document.getElementById("course_id");
 var selectedValue = document.getElementById("selectBox");
 
+const btn = document.querySelector('.form-control.btn-primary');
+const persons = localStorage.getItem('persons') ? JSON.parse(localStorage.getItem('persons')) : []
+
+
+function changeType() {
+    var id = selectedValue.options[selectBox.selectedIndex].value;
+    if (id == "1") {
+        document.getElementById("facultyContainer").style.display = 'block';
+        document.getElementById("courseContainer").style.display = 'block';
+        document.getElementById("cathedraContainer").style.display = 'none';
+        document.getElementById("positionContainer").style.display = 'none';
+    }
+    else {
+        document.getElementById("facultyContainer").style.display = 'none';
+        document.getElementById("courseContainer").style.display = 'none';
+        document.getElementById("cathedraContainer").style.display = 'block';
+        document.getElementById("positionContainer").style.display = 'block';
+    }
+}
 
 class Human {
     constructor(name, surname, birthday, phone, location) {
@@ -62,7 +80,7 @@ class Human {
     set location(location) {
         this.location = location;
     }
-}   
+}
 
 class Student extends Human {
     constructor(name, surname, birthday, phone, location, faculty, course) {
@@ -71,7 +89,7 @@ class Student extends Human {
         this._faculty = faculty;
         this._course = course;
     }
-    
+
     get faculty() {
         return this._faculty;
     }
@@ -99,16 +117,12 @@ class Student extends Human {
     }
 }
 
-var student1 = new Student(name_id.value, surname_id.value, birth_date_id.value, phone_id.value, selectedTown, faculty_id.value, course_id.value);
-
-console.log(student1);
-
 class Teacher extends Human {
     constructor(name, surname, birthday, phone, location, cathedra, position) {
-    super(name, surname, birthday, phone, location);
+        super(name, surname, birthday, phone, location);
 
-    this._cathedra = cathedra;
-    this._position = position;
+        this._cathedra = cathedra;
+        this._position = position;
     }
 
     get position() {
@@ -138,14 +152,34 @@ class Teacher extends Human {
     }
 }
 
-const teacher1 = new Teacher();
-
-const btn = document.querySelector('.form-control.btn-primary');
+changeType()
 
 btn.addEventListener('click', () => {
-  localStorage.setItem('student', JSON.stringify(student1));
+    let person;
 
-  var student1 = new Student(name_id.value, surname_id.value, birth_date_id.value, phone_id.value, selectedTown, faculty_id.value, course_id.value);
+    if (selectedValue.value === 1) {
+        person = new Student(
+            name_id.value,
+            surname_id.value,
+            birth_date_id.value,
+            phone_id.value,
+            selectedTown,
+            faculty_id.value,
+            course_id.value
+        );
+    } else {
+        person = new Teacher(
+            name_id.value,
+            surname_id.value,
+            birth_date_id.value,
+            phone_id.value,
+            selectedTown,
+            faculty_id.value,
+            course_id.value
+        );
+    }
 
-  console.log(student1);
+    persons.push(person)
+
+    localStorage.setItem('persons', JSON.stringify(persons));
 });
